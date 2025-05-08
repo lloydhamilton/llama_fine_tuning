75f546d2faed4a726cbfa3e02db2d79ab2ef4df9
lloydhamilton <lloydhamilton1991@gmail.com>
2025-05-08 16:59:38 +0100

    feat: added evaluator

---
+++ Makefile
@@ -0,0 +1,13 @@
+# Variables
+MODEL_URI := runs:/c48f992d2c614b12b751174d09f0e8a6/model
+DOCKER_NAME := llama-3.2-3b-instruct
+
+# Default target
+.PHONY: all
+all: build-docker
+
+# Build docker image for the MLflow model
+.PHONY: build-docker
+build-docker:
+	@echo "Building Docker image for $(DOCKER_NAME)..."
+	mlflow models build-docker --model-uri "$(MODEL_URI)" --name "$(DOCKER_NAME)"
---
+++ src/evaluate/evaluate.py
@@ -0,0 +1,74 @@
+import asyncio
+import os
+import uuid
+
+import mlflow
+import pandas as pd
+from datasets import load_dataset
+from dotenv import load_dotenv
+from transformers.pipelines.base import Dataset
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "../../.env"))
+
+
+class EvaluateLLMModel:
+
+    def __init__(self, model_uri: str, model_name: str):
+        """Initialize the EvaluateLLMModel class.
+
+        Args:
+            model_uri (str): The URI of the model to evaluate.
+        """
+        self._model_name = model_name
+        self._model_uri = model_uri
+        self.val_squad_dataset = load_dataset("squad", split="validation")
+
+    @property
+    def model_uri(self) -> str:
+        """Return the model URI."""
+        return self._model_uri
+
+    @property
+    def model_name(self) -> str:
+        """Return the model name."""
+        return self._model_name
+
+    def process_dataset(self, data: Dataset) -> pd.DataFrame:
+        squad_pd = pd.DataFrame(data[:5])
+        answers = pd.json_normalize(squad_pd["answers"])
+        squad_pd["answers"] = answers["text"]
+        squad_pd["role"] = "user"
+        squad_pd["content"] = squad_pd["question"]
+        return squad_pd
+
+    async def eval_experiment(self):
+        uid = uuid.uuid4().hex[:6]
+        with mlflow.start_run(
+            run_name=f"{self.model_name}-qa-{uid}", tags={"id": uid}
+        ) as run:
+            mlflow.log_param("model_uri", self.model_uri)
+            mlflow.log_param("model_name", self.model_name)
+            eval_dataset = mlflow.data.from_pandas(
+                self.process_dataset(self.val_squad_dataset),
+                source="squad_dataset",
+                name="squad_dataset",
+            )
+            mlflow.log_input(eval_dataset, context="llm_evaluation")
+            mlflow.evaluate(
+                model=self.model_uri,
+                data=eval_dataset,
+                # targets="answers",
+                model_type="question-answering",
+                evaluators="default",
+                extra
+                # how to load transformer model from mlflow.
+                # see -> https://www.mlflow.org/docs/latest/llms/transformers/tutorials/fine-tuning/transformers-peft/
+            )
+
+
+if __name__ == "__main__":
+    # Example usage
+    model_uri = "runs:/a283232fc9284556b2ae75053b0318fb/model"  # 1B model
+    model_name = "Llama-3.2-1B-Instruct"
+    evaluator = EvaluateLLMModel(model_uri, model_name)
+    asyncio.run(evaluator.eval_experiment())
